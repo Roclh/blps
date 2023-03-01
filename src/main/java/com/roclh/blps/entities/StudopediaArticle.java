f4a2cf430e33843c16620352e21ab2b2d3c8bddc
“Nikita <nkolesnikov@devexperts.com>
2023-03-01 18:37:31 +0300

    Added api model property for article and Category

--- src/main/java/com/roclh/blps/entities/StudopediaArticle.java
+++ src/main/java/com/roclh/blps/entities/StudopediaArticle.java
@@ -1,5 +1,6 @@
 package com.roclh.blps.entities;
 
+import io.swagger.annotations.ApiModelProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,15 +16,19 @@ public class StudopediaArticle {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "studopedia_article_gen")
     @SequenceGenerator(name = "studopedia_article_gen", sequenceName = "studopedia_article_seq")
     @Column(name = "id", nullable = false)
+    @ApiModelProperty(value = "id")
     private Long id;
 
+    @ApiModelProperty(value = "name")
     private String name;
 
     @ManyToOne
     @JoinColumn(name = "category_id")
+    @ApiModelProperty(value = "category")
     private Category category;
 
 
+    @ApiModelProperty(value = "content")
     private String content;
 
 
